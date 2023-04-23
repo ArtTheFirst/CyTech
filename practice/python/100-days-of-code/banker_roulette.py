e36a8447ef99cb42709142e39e901a9b6ac470f6
ArtTheFirst <roberts.protege277@gmail.com>
2023-04-23 13:07:05 +0100

    banker roulette

---
+++ practice/python/100-days-of-code/banker_roulette.py
@@ -0,0 +1,14 @@
+import random 
+
+
+names_string = input("Give me everybody's names, separated by a comma. ")
+
+names = names_string.split(", ")
+
+people_count = len(names)
+
+dice_roll = random.randint(0, people_count - 1)
+
+whos_to_pay = names[dice_roll]
+
+print(f"{whos_to_pay} is going to buy the meal today!")
