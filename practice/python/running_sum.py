860dc2b4676878ffa55d9a0498448c3ed4d00c0d
ArtTheFirst <roberts.protege277@gmail.com>
2023-04-29 00:29:30 +0100

    coding practice

--- practice/python/running_sum.py
+++ practice/python/running_sum.py
@@ -1,6 +1,8 @@
 n_result = 0
-n_digit = input("enter number: ")
-while int(n_digit) > 0:
-    n_result += int(n_digit)
-    #print(n_result)
-stop
+n_digit = input("enter numbers: ").split()
+for n in n_digit:
+    if int(n) > 0:
+        n_result += int(n)
+        print(n_result)
+    else:
+        print(n_result)
