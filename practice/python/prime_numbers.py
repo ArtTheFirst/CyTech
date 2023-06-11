ac2cd7a350d0c15a695dab6fefca7a259da4a27c
ArtTheFirst <roberts.protege277@gmail.com>
2023-06-11 05:54:49 +0000

    prime numbers

---
+++ practice/python/prime_numbers.py
@@ -0,0 +1,15 @@
+def is_prime(num):
+    if num < 2: # Zero and negative numbers are not prime
+        return False
+    for i in range(2, int(num**(1/2))+1):
+        if num % i == 0:
+            return False
+    return True
+    
+def generate_primes(n):
+    primes = []
+    for num in range(2, n):
+        if is_prime(num):
+            primes.append(num)
+            return primes
+    print(generate_primes(20))
